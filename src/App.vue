10bf0762eb679db3bfe58d161451ffaee285238f
dohee <dohee.seong@itam.games>
2019-05-12 14:30:17 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      {{$store.state}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+  created() {
+    console.log(this.$store);
+    this.$store.dispatch('moduleA/setData', 'test');
+  }
+}
+</script>
+
